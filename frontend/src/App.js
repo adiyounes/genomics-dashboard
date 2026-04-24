34ea696dd13453ad02b1477baeb7df1308e7fc7e
adiyounes <younesadi18@gmail.com>
2026-04-24 18:34:28 +0200

    Nav bar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom'
+import UploadPage from './pages/UploadPage'
+import ResultsPage from './pages/ResultsPage'
+import StatsPage from './pages/StatsPage'
+import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>Home</div>}/>
-      </Routes>
+      <nav>
+        <NavLink to="/">Upload</NavLink>
+        <NavLink to="/results">Results</NavLink>
+        <NavLink to="/stats">Stats</NavLink>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<UploadPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }

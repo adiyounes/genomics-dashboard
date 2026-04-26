a9d2703619fd8d03a0c4e5186ee72563d14e53be
adiyounes <younesadi18@gmail.com>
2026-04-26 21:14:07 +0200

    setting up uploadpage

--- frontend/src/pages/UploadPage.jsx
+++ frontend/src/pages/UploadPage.jsx
@@ -1,14 +1,21 @@
-import {useStats} from 'react'
+import {useState} from 'react'
 
 export default function UploadPage() {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [file, setFile] = useState(null)
 
-    retutn (
+    function handleSubmit() {
+        console.log(username, email, file)
+    }
+
+    return (
         <div>
             <h1>Upload VCF File</h1>
-            
+            <input type="text" onChange={e => setUsername(e.target.value)}/>
+            <input type="text" onChange={e => setEmail(e.target.value)}/>
+            <input type="file" onChange={e => setFile(e.target.files[0])}/>
+            <button onClick={handleSubmit()}>Analyse</button>
         </div>
     )
 }

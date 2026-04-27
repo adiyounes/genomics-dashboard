5df5c77b8225002279b2af940546bdd2f35f3e2b
adiyounes <younesadi18@gmail.com>
2026-04-27 21:00:00 +0200

    result page added

--- frontend/src/pages/ResultsPage.jsx
+++ frontend/src/pages/ResultsPage.jsx
@@ -1,3 +1,16 @@
+import { useState, useEffect } from 'react'
+import { getUploads } from '../api/client'
+
 export default function ResultsPage() {
-    return <div>Results Page</div>
+    const [uploads, setUploads] = useState([])
+    const [selectedId, setSelectedId] = useState(null)
+
+    useEffect(()=>{
+
+    })
+    return (
+    <div>
+        <h1>Browse Result</h1>
+    </div>
+    )
 }

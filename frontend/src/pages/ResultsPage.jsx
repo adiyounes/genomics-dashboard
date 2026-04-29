fb639283ddebedefd1399d8385c2a8184211d9dd
adiyounes <younesadi18@gmail.com>
2026-04-29 19:44:16 +0200

    result page table and selected element added

--- frontend/src/pages/ResultsPage.jsx
+++ frontend/src/pages/ResultsPage.jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from 'react'
-import { getUploads } from '../api/client'
+import { getUploads, getVariants, getRiskSummary } from '../api/client'
 
 export default function ResultsPage() {
     const [uploads, setUploads] = useState([])
     const [selectedId, setSelectedId] = useState(null)
+    const [variants, setVariant] = useState([])
+    const [summary, setSummary] = useState(null)
 
     useEffect(()=>{
         getUploads().then(data => setUploads(data))
     }, [])
+
+    useEffect(()=> {
+        if(!selectedId) return
+        getVariants(selectedId).then(data => setVariant(data))
+        getRiskSummary(selectedId).then(data=>setSummary([0]))
+    },[selectedId])
+
     return (
     <div>
         <h1>Browse Result</h1>
@@ -18,6 +27,40 @@ export default function ResultsPage() {
                 </option>
             ))}
         </select>
+        {summary && (
+            <div>
+                <h2>Risk Summary</h2>
+                <p>Overall: {summary.overall_score}</p>
+                <p>Pathogenicity: {summary.pathogenicity_score}</p>
+                <p>Pharmacogenomics: {summary.pharmacogenomics_score}</p>
+            </div>
+        )}
+        {variants.length > 0 && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Gene</th>
+                            <th>Chromosome</th>
+                            <th>Position</th>
+                            <th>Zygosity</th>
+                            <th>Flag</th>
+                            <th>Risk Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {variants.map(v => (
+                            <tr key={v.variant_id}>
+                                <td>{v.gene_name}</td>
+                                <td>{v.chromosome}</td>
+                                <td>{v.position}</td>
+                                <td>{v.zygosity}</td>
+                                <td>{v.flag}</td>
+                                <td>{v.risk_score}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
     </div>
     )
 }

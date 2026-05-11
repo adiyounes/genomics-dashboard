ebd4e75dce4d6117e3605f0a564fe8b639c3ce53
adiyounes <younesadi18@gmail.com>
2026-05-11 10:12:36 +0200

    adding data base credentials

---
+++ database/credentials.py
@@ -0,0 +1,10 @@
+import os
+
+DB_CONFIG = {
+    "host"     : os.getenv("DB_HOST", "localhost"),
+    "database" : os.getenv("DB_NAME", "genomics_db"),
+    "user"     : os.getenv("DB_USER", "genomics_user"),
+    "password" : os.getenv("DB_PASSWORD", "genomics123"),
+    "port"     : os.getenv("DB_PORT", "5432"),
+    "sslmode": os.getenv("DB_SSLMODE", "disable")
+}
